3a88332dc44b7831155b4513a76a722fb2d2c216
Lukasz <lukasz.parda@gmail.com>
2025-10-01 14:26:37 +0200

    fixing chapters dir placement

--- src/books/naszyjnikCzterechZywiolow/meta.ts
+++ src/books/naszyjnikCzterechZywiolow/meta.ts
@@ -1,4 +1,4 @@
-import Chapter1 from "../Chapters/Chapter1";
+import Chapter1 from "./Chapters/Chapter1";
 
 export const meta = {
     "id": 'naszyjnik-czterech-zywiolow',
@@ -9,11 +9,11 @@ export const meta = {
     "status": "writing",
     "chapters": {
         1: { "id": 1, "title": "Rozdział 1: Znaleziony naszyjnik", Component: Chapter1 },
-        2: { "id": 2, "title": "Rozdział 2: Pierwsze spotkanie z magią" },
-        3: { "id": 3, "title": "Rozdział 3: Podróż do krainy żywiołów" },
-        4: { "id": 4, "title": "Rozdział 4: Spotkanie z opiekunami" },
-        5: { "id": 5, "title": "Rozdział 5: Próba sił" },
-        6: { "id": 6, "title": "Rozdział 6: Powrót do domu" }
+        // 2: { "id": 2, "title": "Rozdział 2: Pierwsze spotkanie z magią" },
+        // 3: { "id": 3, "title": "Rozdział 3: Podróż do krainy żywiołów" },
+        // 4: { "id": 4, "title": "Rozdział 4: Spotkanie z opiekunami" },
+        // 5: { "id": 5, "title": "Rozdział 5: Próba sił" },
+        // 6: { "id": 6, "title": "Rozdział 6: Powrót do domu" }
 
     }
 };
